4cc74933a63ea8aa4c8145be2c7b2664c34b30a0
Yakuba001 <2767yakuba@gmail.com>
2026-09-24 09:58:12 +0300

    chore(dependencies): add list of deps and main class

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,35 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.2.3"
+    id("io.spring.dependency-management") version "1.1.4"
 }
 
 group = "com.yakuba"
 version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    runtimeOnly("org.postgresql:postgresql")
+
+    implementation("org.springframework.boot:spring-boot-starter-aop")
+    implementation("io.github.resilience4j:resilience4j-spring-boot3:2.2.0")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-testcontainers")
+    testImplementation("org.testcontainers:postgresql")
+    testImplementation("org.wiremock:wiremock-standalone:3.4.2")
 }
 
 tasks.test {
---
+++ src/main/java/com/yakuba/RateLimitApp.java
@@ -0,0 +1,11 @@
+package com.yakuba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateLimitApp {
+    public static void main(String[] args) {
+        SpringApplication.run(RateLimitApp.class, args);
+    }
+}
